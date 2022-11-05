5041a8f63050c1eb3276e83e6a97f5c117105cbe
oscar51011 <oscar51011@gmail.com>
2022-11-05 20:35:11 +0800

    edit redis String service

--- src/main/java/com/redis/service/RedisStringTypeService.java
+++ src/main/java/com/redis/service/RedisStringTypeService.java
@@ -1,8 +1,5 @@
 package com.redis.service;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -12,6 +9,7 @@ import com.redis.exception.RedisException;
 
 /**
  * String Type 一般操作 redis 服務
+ * 
  * 
  * @author oscar51011
  *
